b90f424dd49c47396f91a450be0d64920b74faf2
programador01 <programador01@montespana.com.ni>
2020-03-31 11:38:33 -0600

    Initial Commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("[Controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Value>>> Get(int id)
+        {
+            var values = await _context.Values.FindAsync(id);
+            return Ok(values);
+        }
+    }
+
+
+}
